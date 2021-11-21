4e786bd4ffbefb04d6d4da79c9cbb374c4244b03
Jitka Badhan Nebeská <jitka.badhannebeska@vlmedia.cz>
2021-11-21 14:30:18 +0100

    Templates

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy('images');
+    eleventyConfig.addPassthroughCopy('/images');
     eleventyConfig.addPassthroughCopy('css');
     
     return {
